571953bb0d17b495eab1c209b68897175492c12f
“[github]” <ruturajjadhav122@gmail.com>
2024-12-22 14:32:43 +0530

    eight commit

---
+++ props/src/App.jsx
@@ -0,0 +1,18 @@
+import Component from "./Component";
+
+const App = () => {
+  return (
+    <>
+      <Component name="Ruturaj" age="22" />
+      <Component name="Parth" age="22" />
+      <Component name="Aniruddha" age="22" />
+      <Component name="Shamit" age="22" />
+      <Component name="Tanaya" age="22" />
+      <Component name="Tanvi" age="22" />
+      <Component name="Elizabeth" age="22" />
+      <Component name="Komal" age="22" />
+    </>
+  );
+};
+
+export default App;
---
+++ props/src/Component.jsx
@@ -0,0 +1,9 @@
+const Component = (props) => {
+  return (
+    <div>
+      <h1>Hello i am {props.name} and my age is {props.age}</h1>
+    </div>
+  );
+};
+
+export default Component;
